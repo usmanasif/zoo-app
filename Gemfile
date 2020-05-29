bf78030a050e627755bf6bd869bf12aaf46f67c2
Usman Asif <mail.usmanasif@gmail.com>
2020-05-29 05:44:40 -0700

    Adding pg

--- Gemfile
+++ Gemfile
@@ -5,8 +5,6 @@ ruby '2.6.2'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.4', '>= 5.2.4.2'
-# Use sqlite3 as the database for Active Record
-gem 'sqlite3'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
@@ -44,6 +42,7 @@ gem 'paperclip'
 gem 'jquery-datatables'
 gem 'figaro'
 gem 'aws-sdk-s3', '~> 1'
+gem 'pg'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,13 @@
 
 ActiveRecord::Schema.define(version: 2020_05_29_105252) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "animals", force: :cascade do |t|
     t.string "name"
     t.string "description"
-    t.integer "zoo_id"
+    t.bigint "zoo_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "tag"
@@ -32,7 +35,7 @@ ActiveRecord::Schema.define(version: 2020_05_29_105252) do
     t.bigint "image_file_size"
     t.datetime "image_updated_at"
     t.string "uploadable_type"
-    t.integer "uploadable_id"
+    t.bigint "uploadable_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["uploadable_type", "uploadable_id"], name: "index_uploads_on_uploadable_type_and_uploadable_id"
@@ -46,10 +49,6 @@ ActiveRecord::Schema.define(version: 2020_05_29_105252) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "image_file_name"
-    t.string "image_content_type"
-    t.bigint "image_file_size"
-    t.datetime "image_updated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
